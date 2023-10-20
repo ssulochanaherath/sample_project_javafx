74c00571de53c0e107547afcaa7836b33377c507
sangeeth <ssulochanaherath@gmail.com>
2023-10-20 23:06:49 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/Launcher.java
@@ -0,0 +1,26 @@
+package lk.ijse;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/DashboardForm.fxml"))));
+
+        stage.setTitle("Item Form");
+        stage.centerOnScreen();
+
+        stage.show();
+    }
+}
---
+++ src/main/java/lk/ijse/controller/DashboardForm.java
@@ -0,0 +1,43 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
+public class DashboardForm {
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
